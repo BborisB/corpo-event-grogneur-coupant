1732014cc37c4185095583c0390064441761c605
Boris Betremieux <146920742+BborisB@users.noreply.github.com>
2025-01-07 23:36:46 +0100

    feat(src): add methods to CommentaireRepository

--- src/repository/CommentaireRepository.java
+++ src/repository/CommentaireRepository.java
@@ -25,6 +25,43 @@ public class CommentaireRepository {
 	}
 	
 	public void supprimerCommentaire(Commentaire commentaire) {
-		
+		commentaires.remove(commentaire);
 	}
+	
+	/**
+     * Trouve un commentaire par son identifiant.
+     *
+     * @param id l'identifiant du commentaire
+     * @return le commentaire correspondant ou null si non trouvé
+     */
+    public Commentaire trouverParId(int id) {
+        for (Commentaire commentaire : commentaires) {
+            if (commentaire.getId() == id) {
+                return commentaire;
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * Retourne tous les commentaires associés à un evenement
+     * @param evenement l'evenement de recherche
+     * @return la liste de tous les commentaires associés à l'evenement
+     */
+    public List<Commentaire> trouverParEvenement(Evenement evenement) {
+        List<Commentaire> commentaires = new ArrayList<Commentaire>();
+        for(Commentaire commentaire : this.commentaires)
+        	if(commentaire.getEvenement().equals(evenement))
+        		commentaires.add(commentaire);
+    	return commentaires;
+    }
+    
+    /**
+     * Retourne tous les commentaires.
+     *
+     * @return la liste de tous les commentaires
+     */
+    public List<Commentaire> trouverTous() {
+        return commentaires;
+    }
 }
